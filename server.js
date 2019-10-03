1307137ed5d4b9c884288456aa8cdb97e9731571
kawalec <kawalec@gmail.com>
2019-10-03 15:34:52 +0200

    Block pasting direct links in the browser

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-// import http from 'http';
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Naglowek strony Node.js</h1>");
-});
+const port = process.env.PORT || 3000;
 
-server.listen(3000, "127.0.0.1", () => console.log("Serwer: 127.0.0.1:3000"));
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+      res.write("<h1>Strona główna</h1>");
+      res.write('<a href="./users">Link do strony users</a>');
+      res.end();
+    } else if (
+      req.url === "/users" &&
+      typeof req.headers.referer !== "undefined"
+    ) {
+      res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+      res.end("<h1>Strona użytkowników</h1>");
+    } else {
+      res.writeHead(404, { "Content-type": "text/html; charset=utf-8" });
+      res.end(`<h1>Strona ${req.url} nie istnieje</h1>`);
+    }
+  })
+  .listen(port, "127.0.0.1", () =>
+    console.log(`Serwer nasłuchuje pod adresem 127.0.0.1:${port}`)
+  );
